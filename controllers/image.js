dc9437f8c82c2f6f45b4e907422560561f85c2b4
tejicheema <35580518+tejicheema@users.noreply.github.com>
2019-01-29 15:01:58 -0800

    Add files via upload
    Its my smart brain api

---
+++ controllers/image.js
@@ -0,0 +1,28 @@
+const clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'd81b2e1e3037420d911105ebb4e729ed'
+});
+
+const handleApiCall = (req, res) => {
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  		.then(data => {res.json(data);})
+  		.catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) =>{
+	const { id } = req.body;
+db('users').where('id', '=', id)        
+.increment('entries', 1)
+.returning('entries')
+.then(entries => {
+	res.json(entries[0]);
+})
+.catch(err => res.status(400).json('unable to get enteries'))
+}
+
+
+module.exports ={
+	handleImage,
+	handleApiCall
+};
